e19d467df91cd3ad0a9a9b04d91b780c453db961
Sebastian Morales <smorales441@misena.edu.co>
2022-10-17 17:00:41 -0500

    apise

--- src/main/java/yeilux/com/proyecto/Model/Class/inventario/producto/entradapro.java
+++ src/main/java/yeilux/com/proyecto/Model/Class/inventario/producto/entradapro.java
@@ -20,7 +20,7 @@ public class entradapro {
     private Integer id;
 
     // -----------------------FECHA_ENTRADA-----------------------//
-    private String fecha=obtenerFecha();
+    private String fecha = obtenerFecha();
 
     public static String obtenerFecha() {
         String formato = "yyyy-MM-dd HH:mm:ss";
@@ -40,21 +40,82 @@ public class entradapro {
     // ************************************************//
     // -------------Relacion con inventario------------//
     // ************************************************//
-@ManyToOne(fetch = FetchType.LAZY, optional = false)
-@JoinColumn(name = "inventario_id")
-@JsonProperty(access = Access.WRITE_ONLY)
-private inventariopro inventario;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "inventario_id")
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private inventariopro inventario;
 
     // ************************************************//
     // -------------Relacion con empleado--------------//
     // ************************************************//
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-@JoinColumn(name = "empleado_id")
-@JsonProperty(access = Access.WRITE_ONLY)
-private empleado empleado;
+    @JoinColumn(name = "empleado_id")
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private empleado empleado;
 
     // ************************************************//
     // -------------Contructores accesores--------------//
     // ************************************************//
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public inventariopro getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(inventariopro inventario) {
+        this.inventario = inventario;
+    }
+
+    public empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public entradapro(Integer id, String fecha, @NotNull Integer cantidad, @NotNull Boolean estado,
+            inventariopro inventario, yeilux.com.proyecto.Model.Class.empleado.empleado empleado) {
+        this.id = id;
+        this.fecha = fecha;
+        this.cantidad = cantidad;
+        this.estado = estado;
+        this.inventario = inventario;
+        this.empleado = empleado;
+    }
+
+    public entradapro() {
+    }
 
 }
--- src/main/java/yeilux/com/proyecto/Model/Class/inventario/producto/inventariopro.java
+++ src/main/java/yeilux/com/proyecto/Model/Class/inventario/producto/inventariopro.java
@@ -27,26 +27,74 @@ public class inventariopro {
     // -------------Relacion con salida----------------//
     // ************************************************//
     @OneToMany(mappedBy = "inventario", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-private Set<salidapro> salida = new HashSet<>();
+    private Set<salidapro> salida = new HashSet<>();
     // ************************************************//
     // -------------Relacion con entrada---------------//
     // ************************************************//
     @OneToMany(mappedBy = "inventario", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-private Set<entradapro> entrada = new HashSet<>();
+    private Set<entradapro> entrada = new HashSet<>();
     // ************************************************//
     // -------------Relacion con producto--------------//
     // ************************************************//
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-	@JoinColumn(name = "producto_id")
-	@JsonProperty(access = Access.WRITE_ONLY)
-	private producto producto;
-
-    
-    
+    @JoinColumn(name = "producto_id")
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private producto producto;
 
     // ************************************************//
     // -------------Contructores accesores--------------//
     // ************************************************//
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public Set<salidapro> getSalida() {
+        return salida;
+    }
+
+    public void setSalida(Set<salidapro> salida) {
+        this.salida = salida;
+    }
+
+    public Set<entradapro> getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(Set<entradapro> entrada) {
+        this.entrada = entrada;
+    }
+
+    public producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(producto producto) {
+        this.producto = producto;
+    }
+
+    public inventariopro(Integer id, Boolean estado, Set<salidapro> salida, Set<entradapro> entrada,
+            yeilux.com.proyecto.Model.Class.producto.producto producto) {
+        this.id = id;
+        this.estado = estado;
+        this.salida = salida;
+        this.entrada = entrada;
+        this.producto = producto;
+    }
+
+    public inventariopro() {
+    }
 
 }
