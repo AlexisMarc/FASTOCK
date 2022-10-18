7cea6b317afed49d9adeb707df665cacc4de8a3e
naihila jerez <nvjerez@misena.edu.co>
2022-10-17 20:37:38 -0500

    naapi

--- src/main/java/yeilux/com/proyecto/Controller/empresa/EmpresaController.java
+++ src/main/java/yeilux/com/proyecto/Controller/empresa/EmpresaController.java
@@ -1,25 +1,61 @@
 package yeilux.com.proyecto.Controller.empresa;
 
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Optional; 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import yeilux.com.proyecto.Model.Class.empresa.empresa;
+import yeilux.com.proyecto.Model.Class.empresa.especialidad;
+import yeilux.com.proyecto.Model.Interface.Empresa.IEmpresa;
+import yeilux.com.proyecto.Model.Interface.Empresa.IEspecialidad;
 
 
 @RestController
 @SessionAttributes("empresa")
 @RequestMapping("/empresa")
 public class EmpresaController {
+    @Autowired
+    private IEmpresa iempresa;
+
+    @Autowired
+    private IEspecialidad iespecialidad;
 
 // ****************************************//
 // --------------METODO GET----------------//
 // ****************************************//
 
 // --------------LISTAR TODOS--------------//
-
-
+@GetMapping
+	public ResponseEntity<Page<empresa>> empresas(Pageable pageable){
+		return ResponseEntity.ok(iempresa.findAll(pageable));
+	}
 
 // ---------------LISTAR UNO---------------//
-
+@GetMapping("/{id}")
+	public ResponseEntity<empresa> empresa(@PathVariable Integer id){
+		Optional<empresa> empresaOptional = iempresa.findById(id);
+		
+		if(!empresaOptional.isPresent()){
+			return ResponseEntity.unprocessableEntity().build();
+		}
+		
+		return ResponseEntity.ok(empresaOptional.get());
+	}
 
 
 // ****************************************//
@@ -27,14 +63,46 @@ public class EmpresaController {
 // ****************************************//
 
 // ---------------REGISTRAR----------------//
-
-
+@PostMapping
+	public ResponseEntity<empresa> guardarempresa(@Valid @RequestBody empresa empresa){
+		Optional<especialidad> especialidadOptional = iespecialidad.findById(empresa.getEspecialidad().getId());
+		
+		if(!especialidadOptional.isPresent()){
+			return ResponseEntity.unprocessableEntity().build();
+		}
+		
+		empresa.setEspecialidad(especialidadOptional.get());
+		empresa empresaGuardado = iempresa.save(empresa);
+		URI ubicacion = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(empresaGuardado.getId()).toUri();
+		
+		return ResponseEntity.created(ubicacion).body(empresaGuardado);
+	}
 
 // ****************************************//
 // --------------METODO PUT----------------//
 // ****************************************//
 
 // ----------------EDITAR------------------//
+@PutMapping("/{id}")
+	public ResponseEntity<empresa> actualizarempresa(@Valid @RequestBody empresa empresa,@PathVariable Integer id){
+		Optional<especialidad> especialidadOptional = iespecialidad.findById(empresa.getEspecialidad().getId());
+		
+		if(!especialidadOptional.isPresent()){
+			return ResponseEntity.unprocessableEntity().build();
+		}
+		
+		Optional<empresa> empresaOptional = iempresa.findById(id);
+		if(!empresaOptional.isPresent()){
+			return ResponseEntity.unprocessableEntity().build();
+		}
+		
+		empresa.setEspecialidad(especialidadOptional.get());
+		empresa.setId(empresaOptional.get().getId());
+		iempresa.save(empresa);
+		
+		return ResponseEntity.noContent().build();
+	}
 
 
 
--- src/main/java/yeilux/com/proyecto/Controller/producto/ProductoController.java
+++ src/main/java/yeilux/com/proyecto/Controller/producto/ProductoController.java
@@ -1,33 +1,60 @@
 package yeilux.com.proyecto.Controller.producto;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import yeilux.com.proyecto.Model.Class.producto.producto;
+import yeilux.com.proyecto.Model.Interface.producto.IProducto;
 
 @RestController
 @SessionAttributes("producto")
 @RequestMapping("/producto")
 
 public class ProductoController {
+    @Autowired
+    private IProducto iproducto;
 
 // ****************************************//
 // --------------METODO GET----------------//
 // ****************************************//
 
 // --------------LISTAR TODOS--------------//
-
-
+@GetMapping
+	public ResponseEntity<Collection<producto>> listarproductos(){
+		return new ResponseEntity<>(iproducto.findAll(),HttpStatus.OK);
+	}
 
 // ---------------LISTAR UNO---------------//
-
-
+@GetMapping("/{id}")
+	public ResponseEntity<producto> obtenerproductoPorId(@PathVariable Integer id){
+		producto producto = iproducto.findById(id).orElseThrow();
+		
+		if(producto != null) {
+			return new ResponseEntity<>(iproducto.findById(id).orElseThrow(),HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+		}
+	}
 
 // ****************************************//
 // -------------METODO POST----------------//
 // ****************************************//
 
 // ---------------REGISTRAR----------------//
-
+@PostMapping
+	public ResponseEntity<?> guardarproducto(@RequestBody producto producto){
+		return new ResponseEntity<>(iproducto.save(producto),HttpStatus.CREATED);
+	}
 
 
 // ****************************************//
