ca913210196c05572478f4a74181a8f86e923316
Didian <dmadarme5@misena.edu.co>
2022-11-04 15:43:20 -0500

    Subida de archivos

---
+++ src/main/java/yeilux/com/proyecto/Controller/upload/FileController.java
@@ -0,0 +1,64 @@
+package yeilux.com.proyecto.Controller.upload;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import yeilux.com.proyecto.Mapping.upload.File;
+import yeilux.com.proyecto.Messages.upload.Response;
+import yeilux.com.proyecto.Service.upload.FileServiceAPI;
+
+
+
+@RestController
+@RequestMapping("/files")
+public class FileController {
+
+	@Autowired
+	private FileServiceAPI fileServiceAPI;
+
+	@PostMapping("/upload")
+	public ResponseEntity<Response> uploadFiles(@RequestParam("files") List<MultipartFile> files) throws Exception {
+		fileServiceAPI.save(files);
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(new Response("Los archivos fueron cargados correctamente al servidor"));
+	}
+
+	@GetMapping("/{filename:.+}")
+	public ResponseEntity<Resource> getFile(@PathVariable String filename) throws Exception {
+		Resource resource = fileServiceAPI.load(filename);
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+				.body(resource);
+	}
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<File>> getAllFiles() throws Exception {
+		List<File> files = fileServiceAPI.loadAll().map(path -> {
+			String filename = path.getFileName().toString();
+			String url = MvcUriComponentsBuilder.fromMethodName(FileController.class, "getFile", path.getFileName().toString()).build().toString();
+			
+			return new File(filename, url);
+		}).collect(Collectors.toList());
+		
+		return ResponseEntity.status(HttpStatus.OK).body(files);
+	}
+
+}
+
+
+
+    
+
---
+++ src/main/java/yeilux/com/proyecto/Service/upload/FileServiceImpl.java
@@ -0,0 +1,44 @@
+package yeilux.com.proyecto.Service.upload;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileServiceImpl implements FileServiceAPI {
+    private final Path rootFolder = Paths.get("uploads");
+
+	@Override
+	public void save(MultipartFile file) throws Exception {
+		Files.copy(file.getInputStream(), this.rootFolder.resolve(file.getOriginalFilename()));
+	}
+
+	@Override
+	public Resource load(String name) throws Exception {
+		Path file = rootFolder.resolve(name);
+		Resource resource = new UrlResource(file.toUri());
+		return resource;
+	}
+
+	@Override
+	public void save(List<MultipartFile> files) throws Exception {
+		for (MultipartFile file : files) {
+			this.save(file);
+		}
+	}
+
+	@Override
+	public Stream<Path> loadAll() throws Exception {
+		return Files.walk(rootFolder, 1).filter(path -> !path.equals(rootFolder)).map(rootFolder::relativize);
+	}
+
+    
+
+}
